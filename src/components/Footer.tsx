de8ba504968cbc19266ecfc322e14f9252f5f88f
Viper <pruncle21@gmail.com>
2026-04-21 19:43:55 -0500

    Organized the text-content of the webpage into different functional components that are within the components directory. The functional components are imported into App.tsx where they are then rendered to the webpage.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+function PhoneFooter() {
+    return (
+        <div>
+            <footer className="attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+                Coded by <a href="#">Your Name Here</a>.
+            </footer>
+        </div>
+    )
+}
+
+export default function Footer() {
+    return (
+        <>
+            <PhoneFooter />
+        </>
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+function PhoneHeader() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Shop</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default function Header() {
+    return (
+        <>
+            <PhoneHeader />
+        </>
+    )
+}
