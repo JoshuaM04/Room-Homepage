217e764a04fd2b9cf920b251265055984c5a87d4
Viper <pruncle21@gmail.com>
2026-04-26 14:11:44 -0500

    Added various functional components from React Aria to wrap the navigation list for the website. Revised some default styling for the buttons and added some inner child containers to adjust alignment of content.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,9 @@ import iconHamburger from "../assets/images/icon-hamburger.svg";
 import iconAngleLeft from "../assets/images/icon-angle-left.svg";
 import iconAngleRight from "../assets/images/icon-angle-right.svg";
 import { shiftGalleryLeft, shiftGalleryRight } from "../utils/helper";
+import {DialogTrigger} from 'react-aria-components/Modal';
 import {Modal} from '../utils/Header/Modal.tsx';
+import {Dialog} from '../utils/Header/Dialog.tsx';
 import {Button} from '../utils/Header/Button.tsx';
 import { useState } from 'react';
 
@@ -24,25 +26,29 @@ interface HeaderProps {
 
 function PhoneHeader({isMenuHidden, setIsMenuHidden, isHeadingHidden, setIsHeadingHidden, galleryList, index, setIndex}: LayoutProps) {
     return (
-        <div className={`[ header_parent_container ] [ p-5 ] [ h-full ] [ ${galleryList[index]} bg-center bg-no-repeat bg-cover ] [ relative ]`}>
-            <section id="drop-down menu" className="[ flex items-center justify-between min-w-screen relative left-0 ] [ bg-white ] [ p-[40px_20px_40px_20px] -m-5 ]" hidden={isMenuHidden} aria-hidden={isMenuHidden} aria-expanded={!(isMenuHidden)}>
-                <button onClick={() => { setIsMenuHidden(true); setIsHeadingHidden(false); }} aria-controls="drop-down menu" aria-label="Close drop-down menu"><img src={iconClose} className="min-w-4 h-4" aria-hidden="true" /></button>
-
-                <nav>
-                    <ul className="[ inline-flex gap-6 ] [ font-bold ]">
-                        <li>home</li>
-                        <li>shop</li>
-                        <li>about</li>
-                        <li>contact</li>
-                    </ul>
-                </nav>
-            </section>
-            
-            <div className="[ flex justify-between ] [ text-white font-semibold text-2xl ] [ w-full h-10 ]" hidden={isHeadingHidden} aria-hidden={isHeadingHidden}>
-                <button onClick={() => { setIsMenuHidden(false); setIsHeadingHidden(true); }} aria-label="Open drop-down menu" aria-controls="drop-down menu" aria-haspopup="true"><img src={iconHamburger} aria-hidden="true" /></button>
-                <p>room</p>
-                <div className="hidden_alignment_container w-5 h-6" aria-hidden="true"></div>
-            </div>
+        <div className={`[ header_parent_container ] [ p-5 ] [ h-full ] [ ${galleryList[index]} bg-center bg-no-repeat bg-cover ] [ relative ]`}> 
+                <DialogTrigger>
+                    <div className="[ flex justify-between ] [ text-white font-semibold text-2xl ] [ w-full h-10 ]">
+                        <Button><img src={iconHamburger} aria-hidden="true" /></Button>
+                        <p>room</p>
+                        <div className="hidden_alignment_container w-5 h-6" aria-hidden="true"></div>
+                    </div>
+                    <Modal>
+                        <Dialog>
+                            <div className="flex justify-between items-center">
+                                <Button slot="close"><img src={iconClose} className="min-w-4 h-4" aria-hidden="true" /></Button>
+                                <nav>
+                                    <ul className="[ inline-flex gap-6 ] [ font-bold ]">
+                                        <li>home</li>
+                                        <li>shop</li>
+                                        <li>about</li>
+                                        <li>contact</li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </Dialog>
+                    </Modal>
+                </DialogTrigger>
 
             <div className="[ flex ] [ w-fit ] [ absolute bottom-0 right-0 ]">
                 <button onClick={() => shiftGalleryLeft(index, setIndex)} className="[ flex justify-center items-center ] [ bg-black ] [ w-15 h-15 ] [ active:bg-gray-600 ]" aria-label="Move back through gallery selection"><img src={iconAngleLeft} aria-hidden="true" /></button>
