50bf9dac80ec3997f0cfa15faaeef961e1aeb1e9
Viper <pruncle21@gmail.com>
2026-04-26 00:38:54 -0500

    Added respective imports and utility files to set up the foundations of using functional components that provide accessiblity for various users.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,8 @@ import iconHamburger from "../assets/images/icon-hamburger.svg";
 import iconAngleLeft from "../assets/images/icon-angle-left.svg";
 import iconAngleRight from "../assets/images/icon-angle-right.svg";
 import { shiftGalleryLeft, shiftGalleryRight } from "../utils/helper";
+import {Modal} from '../utils/Header/Modal.tsx';
+import {Button} from '../utils/Header/Button.tsx';
 import { useState } from 'react';
 
 interface LayoutProps {
@@ -22,17 +24,19 @@ interface HeaderProps {
 
 function PhoneHeader({isMenuHidden, setIsMenuHidden, isHeadingHidden, setIsHeadingHidden, galleryList, index, setIndex}: LayoutProps) {
     return (
-        <section className={`[ header_parent_container ] [ p-5 ] [ h-full ] [ ${galleryList[index]} bg-center bg-no-repeat bg-cover ] [ relative ]`}>
-            <dialog id="drop-down menu" className="[ flex items-center justify-between ] [ bg-white ] [ p-[40px_20px_40px_20px] -m-5 ]" hidden={isMenuHidden} aria-hidden={isMenuHidden} aria-expanded={!(isMenuHidden)}>
+        <div className={`[ header_parent_container ] [ p-5 ] [ h-full ] [ ${galleryList[index]} bg-center bg-no-repeat bg-cover ] [ relative ]`}>
+            <section id="drop-down menu" className="[ flex items-center justify-between min-w-screen relative left-0 ] [ bg-white ] [ p-[40px_20px_40px_20px] -m-5 ]" hidden={isMenuHidden} aria-hidden={isMenuHidden} aria-expanded={!(isMenuHidden)}>
                 <button onClick={() => { setIsMenuHidden(true); setIsHeadingHidden(false); }} aria-controls="drop-down menu" aria-label="Close drop-down menu"><img src={iconClose} className="min-w-4 h-4" aria-hidden="true" /></button>
 
-                <ul className="[ inline-flex gap-6 ] [ font-bold ]">
-                    <li>home</li>
-                    <li>shop</li>
-                    <li>about</li>
-                    <li>contact</li>
-                </ul>
-            </dialog>
+                <nav>
+                    <ul className="[ inline-flex gap-6 ] [ font-bold ]">
+                        <li>home</li>
+                        <li>shop</li>
+                        <li>about</li>
+                        <li>contact</li>
+                    </ul>
+                </nav>
+            </section>
             
             <div className="[ flex justify-between ] [ text-white font-semibold text-2xl ] [ w-full h-10 ]" hidden={isHeadingHidden} aria-hidden={isHeadingHidden}>
                 <button onClick={() => { setIsMenuHidden(false); setIsHeadingHidden(true); }} aria-label="Open drop-down menu" aria-controls="drop-down menu" aria-haspopup="true"><img src={iconHamburger} aria-hidden="true" /></button>
@@ -44,7 +48,7 @@ function PhoneHeader({isMenuHidden, setIsMenuHidden, isHeadingHidden, setIsHeadi
                 <button onClick={() => shiftGalleryLeft(index, setIndex)} className="[ flex justify-center items-center ] [ bg-black ] [ w-15 h-15 ] [ active:bg-gray-600 ]" aria-label="Move back through gallery selection"><img src={iconAngleLeft} aria-hidden="true" /></button>
                 <button onClick={() => shiftGalleryRight(index, setIndex)} className="[ flex justify-center items-center ] [ bg-black ] [ w-15 h-15 ] [ active:bg-gray-600 ]" aria-label="Move forward through gallery selection"><img src={iconAngleRight} aria-hidden="true" /></button>
             </div>
-        </section>
+        </div>
     )
 }
 
